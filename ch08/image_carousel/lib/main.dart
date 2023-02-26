56fea8a69b7e45cd58c79f7ad9382475d0019db2
Im Dae Lim <blackway76@gmail.com>
2023-02-26 17:11:42 +0900

    20230227 스터디 1

--- ch08/image_carousel/lib/main.dart
+++ ch08/image_carousel/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:image_carousel/screen/home_screen.dart';
+// import 'package:image_carousel/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() {
   runApp(
@@ -8,4 +10,18 @@ void main() {
       home: HomeScreen(),
     ),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [1, 2, 3, 4, 5].map((number) => Image.asset('asset/img/image_$number.jpeg',
+        fit: BoxFit.cover,),).toList(),
+      ),
+    );
+  }
 }
