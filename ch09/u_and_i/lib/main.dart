444f280895bb5765f31cf3b40e2a04dac6959917
Im Dae Lim <blackway76@gmail.com>
2023-03-05 21:46:12 +0900

    홈 스크림 UI 구현하기 (ver 1.0)

--- ch09/u_and_i/lib/main.dart
+++ ch09/u_and_i/lib/main.dart
@@ -4,6 +4,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        // 1. 테마를 지정할 수 있는 클랙스
+        fontFamily: 'sunflower',  // 기본 글씨체
+        textTheme: TextTheme(
+          // 2. 글짜 테마를 적용할 수 있는 클랙스
+          headline1: TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'parisienne',
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
--- ch09/u_and_i/lib/screen/home_screen.dart
+++ ch09/u_and_i/lib/screen/home_screen.dart
@@ -30,13 +30,19 @@ class HomeScreen extends StatelessWidget {
 class _CoupleImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // 1. 이미지 중앙 정렬
-      child: Image.asset(
-        'asset/img/middle_image.png',
-        // 2. 화면의 반만큼 높이 구현
-        height: MediaQuery.of(context).size.height / 2,
-      ),
+    return Expanded(
+      // Expanded 추가로 해상도에 따른 오버플로어 해결
+        child: Center(
+          // 1. 이미지 중앙 정렬
+          child: Image.asset(
+            'asset/img/middle_image.png',
+            // 2. 화면의 반만큼 높이 구현
+            height: MediaQuery
+                .of(context)
+                .size
+                .height / 2,
+          ),
+        ),
     );
   }
 }
@@ -44,33 +50,44 @@ class _CoupleImage extends StatelessWidget {
 class _DDay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 1. 테마 불러오기
+    final textTheme = Theme
+        .of(context)
+        .textTheme;
+
     return Column(children: [
       const SizedBox(height: 16.0),
       Text(
         // 최상단 U&I 글자
         'U&I',
+        style: textTheme.headline1,
       ),
       const SizedBox(height: 16.0),
       Text(
         // 두번째 글자
         '우리 처음 만난 날',
+        style: textTheme.bodyText1,
       ),
       Text(
         // 임시로 지정한 만난 날짜
         '2021.11.23',
+        style: textTheme.bodyText2,
       ),
       const SizedBox(height: 16.0),
       IconButton(
         // 하트 아이콘 버튼
+        iconSize: 60.0,
         onPressed: () {},
         icon: Icon(
           Icons.favorite,
+          color: Colors.red,
         ),
       ),
       const SizedBox(height: 16.0),
       Text(
         // 만난 후 DDay
         'D+365',
+        style: textTheme.headline2,
       )
     ]);
   }
