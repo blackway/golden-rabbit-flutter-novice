91bf0fe9f1c058a0ce8caff838030a99ead6228b
Im Dae Lim <blackway76@gmail.com>
2023-03-05 22:41:45 +0900

    CupertinoDatePicker 변경 값 상태 관리에 적용하기

--- ch09/u_and_i/lib/screen/home_screen.dart
+++ ch09/u_and_i/lib/screen/home_screen.dart
@@ -1,8 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -18,42 +25,70 @@ class HomeScreen extends StatelessWidget {
           // 반채축 최대 크기로 늘리기
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
+            _DDay(
+              // 5. 하트 눌렀을 때 실행할 함수 전달하기
+              onHeartPressed: onHeartPressed,
+              firstDay: firstDay,
+            ),
             _CoupleImage(),
           ],
         ),
       ),
     );
   }
-}
 
-class _CoupleImage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      // Expanded 추가로 해상도에 따른 오버플로어 해결
-        child: Center(
-          // 1. 이미지 중앙 정렬
-          child: Image.asset(
-            'asset/img/middle_image.png',
-            // 2. 화면의 반만큼 높이 구현
-            height: MediaQuery
-                .of(context)
-                .size
-                .height / 2,
+  void onHeartPressed() {
+
+    // 2. 쿠퍼티노 다이얼로그 실행
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            // 4. 날짜 선택하는 다아얼로그
+            child: CupertinoDatePicker(
+              // 5. 시간 제회하고 날짜만 선택하기
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                // 1. 상태 변경 시 setState() 함수 실행
+                setState(() {
+                  // 2. firstDay 변수에서 하루 빼기
+                  firstDay = date;
+                  // firstDay = firstDay.subtract(Duration(days: 1));
+                });
+                print('클릭');
+
+              },
+            ),
           ),
-        ),
+        );
+      },
+      barrierDismissible: true,
     );
   }
 }
 
 class _DDay extends StatelessWidget {
+  // 1. 하트 눌렀을 때 실행할 함수
+  final GestureTapCallback onHeartPressed;
+
+  // 사기귀 시작한 날
+  final DateTime firstDay;
+
+  _DDay({
+    // 2. 상위에서 함수 입력 받기
+    required this.onHeartPressed,
+    required this.firstDay,
+  });
+
   @override
   Widget build(BuildContext context) {
     // 1. 테마 불러오기
-    final textTheme = Theme
-        .of(context)
-        .textTheme;
+    final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
 
     return Column(children: [
       const SizedBox(height: 16.0),
@@ -70,14 +105,15 @@ class _DDay extends StatelessWidget {
       ),
       Text(
         // 임시로 지정한 만난 날짜
-        '2021.11.23',
+        // '2021.11.23',
+        '${firstDay.year}.${firstDay.month}.${firstDay.day}',
         style: textTheme.bodyText2,
       ),
       const SizedBox(height: 16.0),
       IconButton(
         // 하트 아이콘 버튼
         iconSize: 60.0,
-        onPressed: () {},
+        onPressed: onHeartPressed,
         icon: Icon(
           Icons.favorite,
           color: Colors.red,
@@ -86,9 +122,28 @@ class _DDay extends StatelessWidget {
       const SizedBox(height: 16.0),
       Text(
         // 만난 후 DDay
-        'D+365',
+        // 'D+365',
+        'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
         style: textTheme.headline2,
       )
     ]);
   }
 }
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      // Expanded 추가로 해상도에 따른 오버플로어 해결
+      child: Center(
+        // 1. 이미지 중앙 정렬
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          // 2. 화면의 반만큼 높이 구현
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
+    );
+  }
+}
+
